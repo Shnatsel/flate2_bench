cb53c5564f92c27bf65f5720b02b331d30cb030f
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-04-14 00:17:26 +0100

    Actually run fdeflate benchmarks

--- run.sh
+++ run.sh
@@ -19,23 +19,27 @@ rustc +nightly --version --verbose
 echo "\n -- inflate (chunks of 4096 bytes) -- \n"
 
 target/release/flate2_bench_miniz_oxide inflate silesia-small.tar.gz 4096 5 zlib
+target/release/flate2_bench_fdeflate inflate silesia-small.tar.gz 4096 5 zlib
 target/release/flate2_bench_zlib_ng inflate silesia-small.tar.gz 4096 5 zlib
 target/release/flate2_bench_zlib_rs inflate silesia-small.tar.gz 4096 5 zlib
 
 echo "\n -- deflate level 1 (chunks of 4096 bytes) -- \n"
 
 target/release/flate2_bench_miniz_oxide deflate 1 silesia-small.tar 4096 5 zlib
+target/release/flate2_bench_fdeflate deflate 1 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_ng deflate 1 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_rs deflate 1 silesia-small.tar 4096 5 zlib
 
 echo "\n -- deflate level 6 (chunks of 4096 bytes) -- \n"
 
 target/release/flate2_bench_miniz_oxide deflate 6 silesia-small.tar 4096 5 zlib
+target/release/flate2_bench_fdeflate deflate 6 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_ng deflate 6 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_rs deflate 6 silesia-small.tar 4096 5 zlib
 
 echo "\n -- deflate level 9 (chunks of 4096 bytes) -- \n"
 
 target/release/flate2_bench_miniz_oxide deflate 9 silesia-small.tar 4096 5 zlib
+target/release/flate2_bench_fdeflate deflate 9 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_ng deflate 9 silesia-small.tar 4096 5 zlib
 target/release/flate2_bench_zlib_rs deflate 9 silesia-small.tar 4096 5 zlib
